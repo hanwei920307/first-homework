1ad183e9735210374fdb52a1c6cf9917f18f3ec8
= <=>
2013-03-16 17:27:33 +0800

    still work for delivering people name

--- js/application.js
+++ js/application.js
@@ -1,24 +1,31 @@
-function  select_people(){
-var str="";
-for(var i=0;i<users.length;i++)
- {
-    str += '<li><a href="#select_people">'+users[i].name+'</a></li>';
-    $("#select_people").html(str);
- }
-try{
-    $("#select_people").listview('refresh');
+function  select_people()
+{
+    str="";
+    for(var i=0;i<users.length;i++)
+    {
+        str += '<li><a onclick="select_people_end(' + i + ')">'+users[i].name+'</a></li>';
+    }
+    $("#list_people").html(str);
+    try{
+        $("#list_people").listview('refresh');
+    } catch(e) {}
 }
- catch(e){}
+
+function  select_people_end(id)
+{
+    localStorage.people=users[id].name;
 }
+
 function  select_field(){
- var str="";
- for(var i=0;i<restaurants.length;i++)
- {
-     str += '<li><a href="#select_field">'+restaurants[i].name+'</a></li>';
+var str="";
+for(var i=0;i<restaurants.length;i++)
+  {
+     str += '<li><a>'+restaurants[i].name+'</a></li>';
      $("#select_field").html(str);
- }
-try{
+  }
+   try
+   {
     $("#select_field").listview('refresh');
-}
+   }
  catch(e){}
 }
