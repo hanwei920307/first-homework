f64903552122e586473611bfad640ea993eae0e7
= <=>
2013-03-16 09:34:50 +0800

    solve the problem of refresh

--- js/application.js
+++ js/application.js
@@ -1,17 +1,24 @@
-function  selectpeople(){
+function  select_people(){
 var str="";
 for(var i=0;i<users.length;i++)
  {
-    str += '<li><a href="#selectpeople">'+users[i].name+'</a></li>';
-    $("#selectpeople").html(str);
-
+    str += '<li><a href="#select_people">'+users[i].name+'</a></li>';
+    $("#select_people").html(str);
  }
+try{
+    $("#select_people").listview('refresh');
 }
-function  selectfield(){
+ catch(e){}
+}
+function  select_field(){
  var str="";
  for(var i=0;i<restaurants.length;i++)
  {
-     str += '<li><a href="#selectfield">'+restaurants[i].name+'</a></li>';
-     $("#selectfield").html(str);
+     str += '<li><a href="#select_field">'+restaurants[i].name+'</a></li>';
+     $("#select_field").html(str);
  }
+try{
+    $("#select_field").listview('refresh');
+}
+ catch(e){}
 }
